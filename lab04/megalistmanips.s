fc930be9c06f0be0fab14b52ac6ac010ede6f31c
LJH <1306985284@qq.com>
2023-04-01 12:20:08 +0800

    lab 4

--- lab04/megalistmanips.s
+++ lab04/megalistmanips.s
@@ -46,17 +46,15 @@ main:
     ecall
 
 map:
-    addi sp, sp, -12
+    addi sp, sp, -16
     sw ra, 0(sp)
     sw s1, 4(sp)
     sw s0, 8(sp)
-
     beq a0, x0, done    # if we were given a null pointer, we're done.
 
     add s0, a0, x0      # save address of this node in s0
-    add s1, a1, x0      # save address of function in s1
-    add t0, x0, x0      # t0 is a counter
-
+    sw  a1,12(sp)
+    add s1, x0, x0      # t0 is a counter
     # remember that each node is 12 bytes long:
     # - 4 for the array pointer
     # - 4 for the size of the array
@@ -66,28 +64,33 @@ map:
     # are modified by the callees, even when we know the content inside the functions 
     # we call. this is to enforce the abstraction barrier of calling convention.
 mapLoop:
-    add t1, s0, x0      # load the address of the array of current node into t1
-    lw t2, 4(s0)        # load the size of the node's array into t2
+    slli t0,s1,2
+    lw t1,0(s0)
+    add a0,t0,t1
+    lw a0,0(a0)
+    lw a1,12(sp)
+    jalr a1
+    
+    slli t0,s1,2
+    lw t1,0(s0)
+    add t0,t0,t1
+    sw a0,0(t0)
+    
+    addi s1,s1,1
+    lw t1,4(s0)
+    blt s1,t1,mapLoop
 
-    add t1, t1, t0      # offset the array address by the count
-    lw a0, 0(t1)        # load the value at that address into a0
-
-    jalr s1             # call the function on that value.
-
-    sw a0, 0(t1)        # store the returned value back into the array
-    addi t0, t0, 1      # increment the count
-    bne t0, t2, mapLoop # repeat if we haven't reached the array size yet
-
-    la a0, 8(s0)        # load the address of the next node into a0
-    lw a1, 0(s1)        # put the address of the function back into a1 to prepare for the recursion
+    lw a0, 8(s0)        # load the address of the next node into a0
+    lw a1, 12(sp)       # put the address of the function back into a1 to prepare for the recursion
 
     jal  map            # recurse
 done:
     lw s0, 8(sp)
     lw s1, 4(sp)
     lw ra, 0(sp)
-    addi sp, sp, 12
-
+    addi sp, sp, 16
+    ret
+    
 print_newline:
     li a1, '\n'
     li a0, 11
